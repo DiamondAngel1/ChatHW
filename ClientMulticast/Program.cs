df1aa012a8f2dcced997d5d3d0d40a93eed81fd5
DiamondAngel1 <tararukolga78@gmail.com>
2025-05-31 20:30:25 +0300

    First hw

---
+++ ClientMulticast/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
+
+UdpClient sender = new UdpClient();
+sender.EnableBroadcast = true;
+IPEndPoint endPoint = new IPEndPoint(IPAddress.Broadcast, 2009);
+Console.WriteLine("Клієнт запущено");
+string login;
+
+
+while (true){
+    Console.WriteLine("Введіть логін:");
+    login = Console.ReadLine();
+    byte[] sendMessage = Encoding.UTF8.GetBytes($"LOGIN:{login}");
+    sender.Send(sendMessage, sendMessage.Length, endPoint);
+
+    byte[] response = sender.Receive(ref endPoint);
+    string responseMessage = Encoding.UTF8.GetString(response);
+    if (responseMessage.StartsWith("Логін")){
+        Console.WriteLine(responseMessage);
+    }
+    else{
+        Console.WriteLine(responseMessage);
+        break;
+    }
+}
+
+Console.WriteLine($"Тепер ви можете надсилати повідомлення в чаті");
+Task.Run(() => {
+    while (true){
+        try{
+            byte[] data = sender.Receive(ref endPoint);
+            string receiveMessage = Encoding.UTF8.GetString(data);
+            Console.WriteLine(receiveMessage);
+        }
+        catch (SocketException ex){
+            Console.WriteLine($"Помилка отримання повідомлення: {ex.Message}");
+        }
+    }
+});
+
+while (true){
+    string message = Console.ReadLine();
+    byte[] send = Encoding.UTF8.GetBytes($"{message}");
+    sender.Send(send, send.Length, endPoint);
+}
---
+++ MulticastServer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
+
+UdpClient server = new UdpClient(2009);
+server.EnableBroadcast = true;
+IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 2009);
+
+Dictionary<IPEndPoint,string> clientSubscribe = new Dictionary<IPEndPoint, string>();
+HashSet<string> usedLogins = new HashSet<string>();
+
+Console.WriteLine("Сервер запущено. Очікуємо клієнта...");
+
+while (true){
+    var data = server.Receive(ref endPoint);
+    string message = Encoding.UTF8.GetString(data);
+    if (clientSubscribe.ContainsKey(endPoint)){
+        Console.WriteLine($"Повідомлення від {endPoint}: {message}");
+        byte[] dataToSend = Encoding.UTF8.GetBytes($"{clientSubscribe[endPoint]}: {message}");
+        foreach (var client in clientSubscribe){
+            if (!client.Key.Equals(endPoint)){
+                server.Send(dataToSend, dataToSend.Length, client.Key);
+            }
+
+        }
+    }
+    else if (message.StartsWith("LOGIN:")){
+        string login = message.Replace("LOGIN:", "").Trim();
+        if (usedLogins.Contains(login)){
+            byte[] errorMessage = Encoding.UTF8.GetBytes($"Логін '{login}' вже використовується. Спробуйте інший.");
+            server.Send(errorMessage, errorMessage.Length, endPoint);
+            continue;
+        }
+        clientSubscribe[endPoint] = login;
+        usedLogins.Add(login);
+
+        Console.WriteLine($"Клієнт {endPoint} приєднався: {login}");
+        byte[] welcomeMessage = Encoding.UTF8.GetBytes($"✅ {login}, вітаємо у чаті!");
+        server.Send(welcomeMessage, welcomeMessage.Length, endPoint);
+    }
+}
